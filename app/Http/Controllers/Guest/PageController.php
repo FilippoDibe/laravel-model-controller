ef55cc5d387a704cf63f8a9b6713e25cbd048660
filippo cafaro <144951463+FilippoDibe@users.noreply.github.com>
2024-02-19 17:44:38 +0100

    push model e controller

---
+++ app/Http/Controllers/Guest/PageController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers\Guest;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class PageController extends Controller
+{
+    public function index() {
+
+        $movies = Movie :: all();
+
+        return view('pages.index', compact('movies'));
+    }
+}
